ca1923abc0a0d3c94c712534855f5b723a09ab1f
Milix-M <tujikou12dh@gmail.com>
2022-03-22 17:32:12 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,175 @@
+import glob
+import os
+import json
+import tkinter as tk
+from socket import socket, AF_INET, SOCK_DGRAM
+import queue
+
+def findnewvrclog(): #最新のVRCログファイルを取得する関数
+    vrclogdir = os.getenv('LOCALAPPDATA') + 'Low\\VRChat\\VRChat'
+    files = glob.glob(vrclogdir + '\\*.txt')
+    logs = {}
+    for file in files:
+        logs[file] = os.stat(file)
+    logs = sorted(logs.items(), key=lambda x: x[1].st_mtime, reverse=True)
+    return logs[0][0]
+
+def sendtoxsoverlay(content): #XSOverlayに通知を送信する関数
+    PORT = 42069
+    ADDRESS = "127.0.0.1" # 自分に送信
+    s = socket(AF_INET, SOCK_DGRAM)
+    msg = {
+        "messageType": 1,
+        "index": 0,
+        "timeout": 3.0,
+        "height": 100,
+        "opacity": 1,
+        "volume": 0.5,
+        "audioPath": "default",
+        "title": "Join",
+        "content": content,
+        "useBase64Icon": False,
+        "icon": "",
+        "sourceApp": "",
+    }
+    msg_str = json.dumps(msg)
+    s.sendto(msg_str.encode("utf-8"), (ADDRESS, PORT))
+    s.close()
+
+def writejoinlog(writedata): #Joinログを.txtファイルに書き出す関数
+    if os.path.exists('.\\vrcjoinlog.txt'):
+        with open(".\\vrcjoinlog.txt", "a", encoding="utf-8") as f:
+            f.write(writedata)
+    else:
+        with open(".\\vrcjoinlog.txt", "x", encoding="utf-8") as f:
+            f.write(writedata)
+
+def savesettings(updinterval, sendxsoverlay, writelog, restorelogs): #設定をファイルに書き込む関数
+    config["updinterval"] = updinterval
+    config["sendxsoverlay"] = sendxsoverlay
+    config["writelog"] = writelog
+    if restorelogs:
+        if not writelog:
+            config["writelog"] = True
+    config["restorelogs"] = restorelogs
+    with open("config.json", "w") as f:
+        json.dump(config, f, indent=2)
+    loadsettings() #設定を再読み込み
+
+def loadsettings(): #設定を読み込む関数
+    global config
+    #設定ファイルを読み込み開始
+    if os.path.exists('.\\config.json'):
+        f = open('.\\config.json', 'r')
+        config = json.load(f)
+        f.close()
+    else:
+        config = {"updinterval": "1500", "sendxsoverlay": True, "writelog": True, "restorelogs": True}
+        f = open('.\\config.json', 'w')
+        json.dump(config, f, indent=2) #json形式で書き込み
+        f.close()
+    #設定ファイル読み込み終了
+
+def createsettingwin(): #設定ウィンドウを作成する関数
+    settingwin = tk.Toplevel()
+    settingwin.title("環境設定")
+    settingwin.geometry("300x200")
+    updintervallabel = tk.Label(settingwin, text="更新間隔(ms)").pack()
+    updinterval = tk.Entry(settingwin, width=10)
+    updinterval.insert(0, config["updinterval"])
+    updinterval.pack()
+    if config["sendxsoverlay"]:
+        bl = tk.BooleanVar()
+        bl.set(True)
+    elif config["sendxsoverlay"] == False:
+        bl = tk.BooleanVar()
+        bl.set(False)
+    sendxsovelaychkbox = tk.Checkbutton(settingwin, variable=bl, text="XSOverlayに送信する").pack()
+    if config["writelog"]:
+        bl2 = tk.BooleanVar()
+        bl2.set(True)
+    elif config["writelog"] == False:
+        bl2 = tk.BooleanVar()
+        bl2.set(False)
+    writelogchkbox = tk.Checkbutton(settingwin, variable=bl2, text="Joinログを.txt形式で書き出す").pack()
+    if config["restorelogs"]:
+        bl3 = tk.BooleanVar()
+        bl3.set(True)
+    elif config["restorelogs"] == False:
+        bl3 = tk.BooleanVar()
+        bl3.set(False)
+    restorelogschkbox = tk.Checkbutton(settingwin, variable=bl3, text="Joinログを.txtファイルから読み込み復元する").pack()
+    complatebuttom = tk.Button(settingwin, text="保存", command=lambda:savesettings(updinterval.get(), bl.get(), bl2.get(), bl3.get())).pack()
+
+def main(lastline): #メイン関数
+    senddatas = queue.Queue()
+    joindata = ""
+    path = findnewvrclog() #最新のVRCログファイルを取得
+    with open(path, encoding="utf-8") as f: #ログファイルをリストで読み込み
+        lines = f.readlines()
+    endlines = len(lines) - 1 #最新の行までの行数
+    newlines = lines[lastline:]
+    for line in newlines:
+        count = line.find("[Behaviour] OnPlayerJoined") #指定文字列がある行を探す
+        if count != -1: #OnPlayerJoinedが見つかったら
+            qdata = (line[60:] + ",").replace("\n", "")
+            senddatas.put(qdata)
+            joindata = line[:19] + " Join"
+    if joindata:
+        joinlog = ""
+        while not senddatas.empty():
+            joinlog = joinlog + senddatas.get()
+        final_string = joindata + joinlog.rstrip(",") + "\n"
+        if config["sendxsoverlay"]:
+            xsoverlaysenddata = joinlog.rstrip(",").lstrip(" ") #XSOverlayに送るデータ
+            sendtoxsoverlay(xsoverlaysenddata) #XSOverlayに送信
+        logview.configure(state='normal')
+        logview.insert('end', final_string)
+        logview.see("end")
+        logview.configure(state='disabled')
+        if config["writelog"]:
+            writejoinlog(final_string)
+    root.after(config["updinterval"], main, endlines) #メイン関数を呼び出し
+
+#GUI設定
+root = tk.Tk()
+root.title("VRChat Join Notifier")
+root.geometry("800x500")
+
+#メニューバー
+menubar = tk.Menu(root)
+root.config(menu=menubar)
+menucfg = tk.Menu(root, tearoff=0)
+menubar.add_cascade(label="設定", menu=menucfg)
+menucfg.add_command(label="環境設定", command=createsettingwin)
+
+logolabel = tk.Label(root, text="VRChat Join Notifier", font=("MSゴシック", "20", "bold")).grid(row=0, column=0) #上部ロゴテキスト
+
+#テキストエリア
+scrollbar_text = tk.Scrollbar(orient="vertical") #スクロールバー
+logview = tk.Text(root, yscrollcommand=scrollbar_text.set, font=("メイリオ", "13")) #Joinログ表示用テキスト
+logview.grid(column=0, row=1, sticky=(tk.N, tk.S, tk.E, tk.W))
+scrollbar_text.config(command=logview.yview)
+scrollbar_text.grid(column=1, row=1, sticky=(tk.N, tk.S, tk.E, tk.W))
+
+#ウィンドウのgrid設定
+root.columnconfigure(0, weight=1)
+root.rowconfigure(1, weight=1)
+
+path = findnewvrclog() #最新のVRCログファイルを取得
+with open(path, encoding="utf-8") as f: #ログファイルをリストで読み込み
+    lines = f.readlines()
+
+loadsettings() #設定ファイルを読み込む
+
+if config["restorelogs"] and os.path.exists(".\\vrcjoinlog.txt"): #Joinログを.txtファイルから読み込み、テキストエリアに表示
+    with open(".\\vrcjoinlog.txt", "r", encoding="utf-8") as f:
+        oldlog = f.read()
+        logview.configure(state='normal')
+        logview.insert('end', oldlog)
+        logview.see("end")
+        logview.configure(state='disabled')
+
+main(len(lines) - 1) #メイン関数を呼び出す
+
+root.mainloop()
