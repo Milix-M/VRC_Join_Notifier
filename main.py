424604cc2c3189e0d14c9ac8290c9cdcee9a36df
Milix-M <tujikou12dh@gmail.com>
2022-04-12 23:22:37 +0900

    メインウィンドウに不要な文字列が表示される不具合の修正

--- main.py
+++ main.py
@@ -229,12 +229,12 @@ def main(lastline): #メイン関数
             xsdata.append((line[60:]).replace("\n", "")) #XSOverlayに送信するデータをリストに追加
             senddatas.put(qdata)
             joindata = line[:19] + " Join"
-        if line.find("[Behaviour] OnPlayerLeft") != -1:
+        if line.find("[Behaviour] OnPlayerLeft ") != -1:
             logview.configure(state='normal')
-            logview.insert('end', line[:19] + " Leave:" + line[59:])
+            logview.insert('end', line[:19] + " Leave" + line[58:])
             logview.see("end")
             logview.configure(state='disabled')
-            writejoinlog(line[:19] + " Leave:" + line[59:])
+            writejoinlog(line[:19] + " Leave" + line[58:])
     if joindata:
         joinlog = ""
         while not senddatas.empty():
